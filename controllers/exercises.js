050d76e4faf3e6658a431cfb2ff1dc39643f5f5e
Charlie Jay <charliejbjay@gmail.com>
2024-06-25 12:46:22 +1000

    logbook and create functionality added

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -3,4 +3,42 @@ const router = express.Router();
 
 const Exercise = require('../models/exercise');
 
+// Home
+router.get('/', async (req, res) => {
+    try {
+        const exercises = await Exercise.find({}).populate('user');
+        console.log(exercises);
+        res.render('exercises/index.ejs', {
+            exercises: exercises
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// New
+router.get('/new', (req, res) => {
+    res.render('exercises/new.ejs');
+});
+
+// Create 
+router.post ('/', async (req, res) => {
+    try {
+        req.body.user = req.session.user._id;
+        await Exercise.create(req.body);
+    } catch (error) {
+        console.log(error);
+    }
+    res.redirect('/exercises');
+})
+
+// Index
+
+// Edit
+
+// Update
+
+// Delete 
+
 module.exports = router;
--- models/exercise.js
+++ models/exercise.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const exerciseSchema = new mongoose.Schema({
     exercise: {
         type: String,
+        enum: ['back squat', 'barbell back rack reverse lunge', 'barbell good morning', 'behind the neck press', 'bench press', 'bent over dumbbell row', 'chin up', 'clean', 'clean pull', 'dumbbell box step up', 'deadlift', 'deficit deadlift', 'dumbbell bench press', 'front squat', 'hang clean', 'hang muscle snatch', 'hang power clean', 'hang power clean + push jerk', 'hang snatch', 'hang power snatch', 'hang squat snatch', 'hip clean', 'hip snatch', 'low hang power clean', 'low hang snatch + overhead squat', 'overhead squat', 'power clean', 'power clean + jerk', 'power snatch', 'push jerk', 'push press', 'romanian deadlift', 'seated barbell strict press', 'single arm dumbbell row', 'snatch', 'strict press', 'pull up', 'bar muscle up', 'ring muscle up', 'strict ring muscle up', 'strict handstand push up'],
         required: true,
     },
     sets: {
@@ -15,11 +16,14 @@ const exerciseSchema = new mongoose.Schema({
     },
     weight: {
         type: Number,
-        required: true,
     },
     notes: {
         type: String,
     },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }
     // TODO: favoritedByUsers
 });
 
