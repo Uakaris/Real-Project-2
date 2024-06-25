ed356bf8d926265608733ca822d57b7f25480b15
Charlie Jay <charliejbjay@gmail.com>
2024-06-25 16:42:02 +1000

    exercises controller and ejs files finished

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -36,9 +36,59 @@ router.post ('/', async (req, res) => {
 // Index
 
 // Edit
-
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.id);
+        res.render('exercises/edit.ejs', {
+            exercise: exercise
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/exercises');
+    }
+});
 // Update
+router.put('/:id', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.id);
+        if (exercise.user.equals(req.session.user._id)) {
+            await exercise.updateOne(req.body);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    res.redirect('/exercises');
+});
 
 // Delete 
+router.delete('/:id', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.id);
+        if (!exercise) {
+            return res.status(404).send('Exercise not found');
+        }
+        // Check if the exercise belongs to the current user
+        if (!exercise.user.equals(req.session.user._id)) {
+            return res.status(403).send('Unauthorized');
+        }
+        await exercise.deleteOne();
+        res.redirect('/exercises');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// router.delete('/:id', async (req, res) => {
+//     try {
+//         const exercise = await Exercise.findById(req.params.id);
+//         if (exercise.user.equals(req.session.user._id)) {
+//             await exercise.deleteOne();
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+//     res.redirect('/exercises');
+// });
 
 module.exports = router;
