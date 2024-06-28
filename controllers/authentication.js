99a61b5a1c67f6bdeed149427b7deefb16d2c010
Charlie Jay <charliejbjay@gmail.com>
2024-06-28 11:34:42 +1000

    changes to sign-in router redirect

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -60,7 +60,7 @@ router.post('/sign-in', async (req, res) => {
       _id: userInDatabase._id
     };
   
-    res.redirect('/');
+    res.redirect('/exercises');
   } catch (error) {
     console.log(error);
     res.redirect('/');
